3f68d85f42e3a5b2f0556ec53a8865acad00d3bf
CaduFelix <50344506+CaduFelix@users.noreply.github.com>
2021-11-10 12:26:34 -0300

    Add files via upload

---
+++ Python-exercicios.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+valor_passagem= 4.30
+
+valor_corrida= input('Qual é o valor da corrida?')
+
+if float(valor_corrida)<= valor_passagem * 5:
+    print ("Pague a corrida")
+else:
+    print ("Pegue o onibus")
+
+
+# In[ ]:
+
+
+
+
